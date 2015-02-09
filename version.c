f0ac09e760ea3eda501892424bb9f00659d00a6b
Alexander Jones <happy5214@gmail.com>
2015-02-08 20:19:08 -0600

    GNU ed version 0.1

---
+++ version.c
@@ -0,0 +1,23 @@
+/* version.c: version definition for the ed editor. */
+/* ed line editor.
+   Copyright (C) 1993 Andrew Moore, Talke Studio
+   All Rights Reserved
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ 	@(#)$Id: version.c,v 1.1 1993/12/22 02:57:59 alm Exp $
+*/
+
+char version_string[] = "GNU ed version 0.1";
